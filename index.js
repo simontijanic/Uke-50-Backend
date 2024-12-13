b2a77426c57da99b2d9a1aee0ea8e91fc9a755fb
simontijanic <simontijanicerkul@gmail.com>
2024-12-13 11:15:07 +0100

    added cors

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 const indexRoute = require("./routes/indexRoute");
 const dbController = require("./controller/dbController");
 
 app.set("view engine", "ejs");
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.json());
 
 app.use(indexRoute);
 
-dbController.connect()
+app.use(
+  cors({
+    origin: "http://10.12.14.120", // Frontend URL
+    methods: ["GET", "POST"], // Allow specific methods
+    allowedHeaders: ["Content-Type"], // Allow specific headers
+  })
+);
+
+dbController
+  .connect()
   .then(() => {
     app.listen(8000, () => {
       console.log("Server is listening on port 8000");
